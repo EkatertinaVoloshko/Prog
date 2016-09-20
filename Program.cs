79c30ffbe122e337af03b3af8bd0f396e91e5752
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-09-21 03:35:11 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число для сложения:");
+            string s = Console.ReadLine();
+            int a = Convert.ToInt32(s);
+
+            Console.WriteLine("Введите второе число для сложения:");
+            string s2 = Console.ReadLine();
+            int b = Convert.ToInt32(s2);
+
+            int c = a + b;
+            Console.WriteLine("Сумма данных чисел:");
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}

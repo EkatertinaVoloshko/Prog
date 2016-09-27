0d1c1e0f5312733144f492f0154417b20f5ecb3c
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-09-27 22:23:56 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("Введите x: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("\nВведите y: ");
+            y = double.Parse(Console.ReadLine());
+            if (x * x + y * y <= 4 && x >= 0 && x <= Math.Sqrt(2) && y >= -2 && y <= Math.Sqrt(2)) Console.Write("\nTrue");
+            else Console.Write("\nFalse");
+            Console.ReadKey();
+        }
+    }
+}

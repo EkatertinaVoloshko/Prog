fcd3acb5534841ecfe24cdb8a2fa6a2edd6413a9
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-09-27 22:30:06 +0300

    Add files via upload

---
+++ Program0.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Делимое:");
+                         string s = Console.ReadLine();
+                        int a = Convert.ToInt32(s);
+            
+
+
+            Console.WriteLine("Введите делитель:");
+                         string s2 = Console.ReadLine();
+                         int b = Convert.ToInt32(s2);
+           
+
+
+            int c = a % b;
+                        Console.WriteLine("Остаток от деления данных чисел:");
+                        Console.WriteLine(c);
+                        Console.ReadLine();
+
+        }
+    }
+}

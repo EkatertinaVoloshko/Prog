615a7cdaecdf46f06c745fe12a0aa07bff6cee69
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-09-27 22:27:05 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите x");
+            string str = Console.ReadLine();
+            double x = double.Parse(str);
+
+            Console.WriteLine("введите y");
+            str = Console.ReadLine();
+            double y = double.Parse(str);
+
+            double g = 0;
+            if (x < y && x > 0)
+            {
+                g = x + Math.Sin(y);
+
+            }
+
+            if (x < y && x < 0)
+            {
+
+                g = y - Math.Cos(x);
+            }
+
+            if (x > y)
+            {
+                g = 0.5 * x * y;
+            }
+
+            Console.WriteLine("Вы ввели x= " + x + " \nи y = " + y + "\n Результат работы программы = " + g);
+            Console.ReadLine();
+
+        }
+    }
+}

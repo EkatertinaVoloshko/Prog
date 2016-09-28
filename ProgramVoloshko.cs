2c90cb559a94c038375312751a225bca5da84cf8
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-09-28 14:52:38 +0200

    Add files via upload

---
+++ ProgramVoloshko.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите число:");
+            a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.Write("Число четное. Результат программы {0}", a / 2);
+            }
+            else
+            {
+                Console.Write("Число нечетное. Результат программы {0}", (a + 1) / 2);
+            }
+            Console.ReadLine();
+            }
+        }  
+    }

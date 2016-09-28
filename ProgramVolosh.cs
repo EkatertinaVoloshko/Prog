4b3a715f8b08b800e275e861abc93bb95f4cc46f
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-09-28 15:07:23 +0200

    Add files via upload

---
+++ ProgramVolosh.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            int a;
+            Console.WriteLine ("Введите оценку ");
+            a = int.Parse(Console.ReadLine());
+            if (a < 3)
+            {
+                Console.WriteLine("Не зачет");
+            }
+            else
+
+            {
+                Console.WriteLine("Зачет");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+        

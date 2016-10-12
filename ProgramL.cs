8addc7187df4c61038d4fda91be391869614e88f
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-10-12 14:58:41 +0200

    Add files via upload

---
+++ ProgramL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo;
+            int cifra;
+            int i;
+            string str;
+            Console.WriteLine("Введите ваше число");
+            str = Console.ReadLine();
+            chislo = int.Parse(str);
+            i = 0;
+            while (chislo >= 10)
+            {
+                cifra = chislo % 10;
+                i = i + 1;
+                str = "Ваша " + i + "-ая цифра";
+                Console.WriteLine(str);
+                Console.WriteLine(cifra);
+                chislo = chislo / 10;
+            }
+            cifra = chislo % 10;
+            i = i + 1;
+            str = "Ваша " + i + "-ая цифра";
+            Console.WriteLine(str);
+            Console.WriteLine(cifra);
+            Console.ReadKey();
+        }
+    }
+}
+    

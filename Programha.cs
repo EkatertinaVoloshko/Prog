26ea80b1eb586b031a5c7f171496142647f35276
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-10-12 15:02:06 +0200

    Add files via upload

---
+++ Programha.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ikl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число :");
+            int a;
+            a = int.Parse(Console.ReadLine());
+            int b;
+            int c, d;
+
+                b = a % 10;
+                c = (a / 10) % 10;
+                d = a / 100;
+                Console.WriteLine("Цифры числа");
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(d);
+                Console.ReadLine();
+
+          
+
+
+        }
+    }
+}

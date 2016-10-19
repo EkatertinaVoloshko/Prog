281949d1a3a7b8a95aee285bdc78f1029a80bf22
EkatertinaVoloshko <ekaterina17voloshko@gmail.com>
2016-10-19 15:53:25 +0300

    Add files via upload

---
+++ Programz2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Задание2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum;
+            int n;
+            n = 1;
+            sum = 0;
+            for (n = 1; n <= 10; n++) ;
+            sum = sum + n * n;
+            Console.Write("Сумма чисел " + sum);
+            Console.ReadLine();
+        }
+    }
+}
+        
---
+++ Programz3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Задание3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0.1;
+            double F = 0;
+           
+            while (x <= 1.0)
+            {
+                F = x + 5;
+                Console.Write("F= " + F);
+                Console.WriteLine(" x= " + x);
+                x = x + 0.1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
